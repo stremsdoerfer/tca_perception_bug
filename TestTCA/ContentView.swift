2cd0b36c4eb2b9b943e26e3926dc430850bb7cc2
Emilien Stremsdoerfer <emstre@verily.com>
2024-04-15 20:42:28 -0700

    Initial Commit

---
+++ TestTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestTCA
+//
+//  Created by Emilien Stremsdoerfer on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestTCA/TestTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTCAApp.swift
+//  TestTCA
+//
+//  Created by Emilien Stremsdoerfer on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
